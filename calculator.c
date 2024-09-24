65d6e4aa84cce6985b2db1dd696509b002b8020a
Nischal Lawot <nischallawot@gmail.com>
2024-09-24 19:58:42 -0400

    created calculator

--- calculator.c
+++ calculator.c
@@ -8,7 +8,45 @@ Author      : Nischal Lawot    lawotnr@dukes.jmu.edu
 File Name   : calculator.c
 -------------------------------------------------------------------------------*/
 
-int main(void)
-{    
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h>
+#include <unistd.h>
 
+int main(int argc, char **argv)
+{    
+    if ( argc != 4 ) 
+    {
+        printf("args: Number Operation Number\n");
+        return -1;
+    }
+    char op = argv[2][0];
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[3]);
+    int result;
+
+    switch ( op )
+    {
+        case '+':   
+            result = num1 + num2 ;
+            break ;
+                    
+        case '-':   
+            result = num1 - num2 ;
+            break ;
+                    
+        case 'x':   
+        case 'X':   
+            result = num1 * num2 ;
+            break ;
+                    
+        case '/':   
+            result = num1 / num2 ;
+            break ;
+
+        default:    
+            printf("\nINVALID operation\n" );
+            result = -9999;
+    } ;
+    printf("%d %c %d = %d\n", num1, op, num2, result);
 }
--- go.c
+++ go.c
@@ -36,5 +36,66 @@ pid_t Fork(void)
 
 int main(void)
 {    
+    pid_t pidLeft, pidRight;
+	int fd[2];
 
+    //printf("Implementing the shell command: cat someFile.txt | more\n" );
+	/** create the pipe */
+	if ( pipe(fd) == -1 ) 
+	{
+        fprintf(stderr,"Pipe failed");
+        return 1;
+	}
+
+    pidLeft = Fork();
+	if ( pidLeft == 0 )   /* Left-Child process */
+	{	/* close the unused end of the pipe */
+        close(  fd[READ_END]   );
+
+        /* overwrite the stdout of Left-Child to the write-end of the pipe */
+        dup2(   fd[WRITE_END]   ,    STDOUT_FD   ) ;
+
+        // start the new executable for the Left-Child
+        if( execlp("./user", "tom", NULL) < 0)
+        {
+            perror("execlp Left-Chiled Failed");
+            exit(-1) ;
+        }
+	}
+	else /* Parent process */
+	{
+        /* now fork the Right-Child process */
+        pidRight = Fork();
+        if ( pidRight == 0)   /* Right-Child process */
+        {
+            /* close the unused end of the pipe */
+            close(   fd[WRITE_END]   );
+
+            /* Overwrite the stdin of Right-Child 
+               to the read-end of the pipe */
+            dup2(    fd[READ_END]   ,    STDIN_FD   ) ;
+
+            // start the new executable for the Left-Child
+            if( execlp("/bin/more", "Jerry" , NULL) < 0 )
+            {
+                perror("execlp Right-Chiled Failed");
+                exit(-1) ;
+            }
+        }
+        else /* Parent process */
+        { 
+            // Parent:  Close both ends of the pipe
+			close(   fd[READ_END]    ) ;
+            close(   fd[WRITE_END]    ) ;
+
+            // Wait for left-child to end
+			waitpid( pidLeft, NULL, 0) ;
+            
+			// wait for right-child to end
+			waitpid(pidRight, NULL, 0 ) ;
+
+            printf("At Your Command #\n" ) ;
+        }
+	}
+	return 0;
 }
